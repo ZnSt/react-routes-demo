54f16e31f1f9b0de1dccdbee4aff9dfca8326962
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2023-02-28 14:54:04 +0200

    add section about

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,9 @@ import { Routes, Route } from 'react-router-dom';
 import { Header } from './Header';
 import { Home } from 'pages/Home';
 import { About } from 'pages/About';
+import { Mission } from './Missions';
+import { Reviews } from './Reviews';
+import { Team } from './Team';
 import { Products } from 'pages/Products';
 import { ProductDetails } from './ProductDetails';
 import { NotFound } from 'pages/NotFound';
@@ -14,7 +17,11 @@ export const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="/products/:id" element={<ProductDetails />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/about" element={<About />}>
+          <Route path="mission" element={<Mission />} />
+          <Route path="reviews" element={<Reviews />} />
+          <Route path="team" element={<Team />} />
+        </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
